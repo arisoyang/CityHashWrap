b8046884bb33d77039f10d18bb219fa3f0882b94
arisoyang <ariso.yang@gmail.com>
2013-03-09 06:19:56 -0500

    VS2010 Clone from https://github.com/gmarz/CityHash

---
+++ CityHash/CityHash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CityHash
+{
+    public static class CityHash
+    {
+        public static UInt32 GetCityHash32(this string value)
+        {
+            return External.CityHash32(new StringBuilder(value), (UInt32)value.Length);
+        }
+
+        public static UInt64 GetCityHash64(this string value)
+        {
+            return External.CityHash64(new StringBuilder(value), (UInt32)value.Length);
+        }
+
+        public static UInt64 GetCityHash64(this string value, UInt64 seed)
+        {
+            return External.CityHash64WithSeed(new StringBuilder(value), (UInt32)value.Length, seed);
+        }
+
+        public static UInt64 GetCityHash64(this string value, UInt64 seed0, UInt64 seed1)
+        {
+            return External.CityHash64WithSeeds(new StringBuilder(value), (UInt32)value.Length, seed0, seed1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 CityHashWrap
 ============
 
-.NET Wrap of Google CityHash
+VS2010 Clone from https://github.com/gmarz/CityHash
+
+.NET wrapper for Google's City Hash family of hash functions (<a href="http://code.google.com/p/cityhash/">http://code.google.com/p/cityhash/</a>).
+
